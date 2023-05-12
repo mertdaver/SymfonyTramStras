8cd86e0dd2cf9950528f0a597b9d3bf36981d5a6
pierrearthurdemengel <pademengel@gmail.com>
2023-05-12 16:05:41 +0200

    liste des alertes depuis BDD ok sym2 47

--- src/Controller/AlerteController.php
+++ src/Controller/AlerteController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Alerte;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AlerteController extends AbstractController
 {
     #[Route('/alerte', name: 'app_alerte')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        $tableauAlertes = ["valeur 1", "valeur 2"];
+        $alertes = $doctrine->getRepository(Alerte::class)->findAll();
+        
         return $this->render('alerte/index.html.twig', [
+            
             'Alerte1' => 'Alice à émit une Alerte dans le tram D direction Kelh',
-            'tableauAlertes' =>  $tableauAlertes ,
+            'alertes' => $alertes,
+            
+        
         ]);
     }
 }
