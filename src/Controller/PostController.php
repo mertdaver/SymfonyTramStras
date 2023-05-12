441ea1016a38edbfac38ab5352c6e1e32fcd6c79
pierrearthurdemengel <pademengel@gmail.com>
2023-05-12 16:54:29 +0200

    Je suis bloqué par le user dans post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Post;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
     #[Route('/post', name: 'app_post')]
     public function index(ManagerRegistry $doctrine): Response
     {
+        $posts = $doctrine->getRepository(Post::class)->findAll();
+
         return $this->render('post/index.html.twig', [
-            'post1' => "Salut, c'est mon premier post ici",
+            'posts' => $posts,
         ]);
     }
 }
