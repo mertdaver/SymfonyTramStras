b742a7c9859385eb2c4865d1cbbc2e3fffd97889
pierrearthurdemengel <pademengel@gmail.com>
2023-06-20 21:11:51 +0200

    API ramènes les points gps dans leaflet

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,28 +2,19 @@
 
 namespace App\Controller;
 
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MapController extends AbstractController
 {
-    private $doctrine;
-
-    public function __construct(ManagerRegistry $doctrine)
-    {
-        $this->doctrine = $doctrine;
-    }
-
     #[Route('/map', name: 'app_map')]
     public function index(): Response
     {
-        $url = 'https://api.cts-strasbourg.eu/v1/siri/2.0/lines-discovery';
+        $url = 'https://api.cts-strasbourg.eu/v1/siri/2.0/stoppoints-discovery';
 
         $username = 'f7e899aa-b4b3-4e27-bdb3-48ff97432546';
         $password = 'Mry78(5kmM_d';
-
 
         $options = [
             'http' => [
@@ -31,27 +22,46 @@ class MapController extends AbstractController
                 'ignore_errors' => true
             ]
         ];
-        
+
         $context = stream_context_create($options);
-
         $response = file_get_contents($url, false, $context);
-
-        // dd($response);
 
         if ($response !== false) {
             $data = json_decode($response, true);
 
-            if (isset($data['LinesDelivery']['AnnotatedLineRef'])) {
+            if (isset($data['StopPointsDelivery']['AnnotatedStopPointRef'])) {
+                $stopPoints = $data['StopPointsDelivery']['AnnotatedStopPointRef'];
+
+                $markers = [];
+                foreach ($stopPoints as $stopPoint) {
+                    $latitude = $stopPoint['Location']['Latitude'];
+                    $longitude = $stopPoint['Location']['Longitude'];
+                    $stopName = $stopPoint['StopName'];
+
+                    // Créer un marqueur pour chaque point
+                    $markers[] = [
+                        'latitude' => $latitude,
+                        'longitude' => $longitude,
+                        'stopName' => $stopName,
+                    ];
+                }
+
+                $lines = [];
+                if (isset($data['LinesDelivery']['AnnotatedLineRef'])) {
+                    $lines = $data['LinesDelivery']['AnnotatedLineRef'];
+                }
+
                 return $this->render('map/index.html.twig', [
-                    'lines' => $data['LinesDelivery']['AnnotatedLineRef'],
+                    'markers' => $markers,
+                    'lines' => $lines,
                 ]);
             } else {
                 var_dump($response);
-                // Handle the error case
+                // Gérer le cas d'erreur
                 return new Response('Failed to retrieve data from the API', 500);
             }
         } else {
-            // Handle the error case
+            // Gérer le cas d'erreur
             return new Response('Failed to retrieve data from the API', 500);
         }
     }
