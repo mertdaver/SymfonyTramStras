27d2fc756633206991d8f012f2d32ed1e5e995b9
pierrearthurdemengel <pademengel@gmail.com>
2023-10-03 13:54:01 +0200

    | raw API CTS text.Value messageCTS\index.html.twig enlevé

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -29,9 +29,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private array $roles = [];
 
-    /**
-     * @var string The hashed password
-     */
     #[ORM\Column]
     private ?string $password = null;
 
